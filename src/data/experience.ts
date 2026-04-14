81b47d3df9c07fc75846c003631a1ae01628da3d
Duy Hoàng <duypo03cbvn@gmail.com>
2026-04-14 22:23:33 +0700

    Cập nhật thông tin

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,7 +9,7 @@ export const experience: ExperienceItem[] = [
   {
     role: "Trợ lý nghiên cứu",
     company: "Bộ môn Khoa học máy tính - Khoa Công nghệ thông tin - (HUCE)",
-    period: "11/2024 - 01/2025",
+    period: "11/2024 - Nay",
     details: [
       "Đọc và tổng hợp các tài liệu liên quan đến đề tài nghiên cứu: Federated Learning, Dataset Distillation...",
       "Cộng tác trong các dự án nghiên cứu và thực tế liên quan đến Trí tuệ nhân tạo (AI) và Học máy (ML).",
